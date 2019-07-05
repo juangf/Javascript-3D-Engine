cbae360e3f25d18986c655dc6db3270687cc1576
Juan García <jgarcia@bodas.net>
2019-07-05 14:51:57 +0200

    feat: new Matrix object

--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,46 @@
+class Matrix {
+    constructor(size = 4) {
+        // Create the identity matrix by default
+        this.values = [];
+        this.size   = 4;
+        for (let i = 0; i < size; i++) {
+            let row = [];
+            for (let j = 0; j < size; j++) {
+                row.push(j === i ? 1 : 0);
+            }
+            this.values.push(row);
+        }
+    }
+    getSize() {
+        return this.size;
+    }
+    get() {
+        return this.values;
+    }
+    getValue(row, col) {
+        return this.values[row][col];
+    }
+    setValue(row, col, value) {
+        this.values[row][col] = value;
+        return this;
+    }
+    static multiply(m1, m2) {
+        let size = m1.getSize();
+        let m    = new Matrix(size);
+        
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                let sum = 0;
+                for (let k = 0; k < size; k++) {
+                    sum += m1.getValue(i, k) * m2.getValue(j, k);
+                }
+                m.setValue(i, j, sum);
+            }
+        }
+        return m;
+    }
+}
+
 class Point {
     constructor(x, y, z) {
         this.x = x;
@@ -6,7 +49,7 @@ class Point {
     }
     setCoords(x, y, z) {
         this.x = x;
-        this.y = y;
+        this.y = y; 
         this.z = z;
         return this;
     }
