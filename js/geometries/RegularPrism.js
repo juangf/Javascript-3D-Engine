b1275688c7ef46f28138cfa3d27d99f55277fad5
Juan García <jgarcia@bodas.net>
2020-04-23 19:36:42 +0200

    feat(regularprism): faces made with triangles by default

--- js/geometries/RegularPrism.js
+++ js/geometries/RegularPrism.js
@@ -14,7 +14,7 @@ class RegularPrism extends Geometry {
      * @param {number} h width
      * @param {number} d dept
      */
-    constructor(w, h, d) {
+    constructor(w, h, d, allTriangles = true) {
         super();
         h = h / 2;
         w = w / 2;
@@ -30,13 +30,29 @@ class RegularPrism extends Geometry {
             .addPoint(new Point(-h, w, d))
             .addPoint(new Point(h, w, d));
 
-        this
-            .addPolygon(new Polygon([0, 1, 2, 3]))
-            .addPolygon(new Polygon([0, 3, 6, 5]))
-            .addPolygon(new Polygon([4, 5, 6, 7]))
-            .addPolygon(new Polygon([1, 4, 7, 2]))
-            .addPolygon(new Polygon([5, 4, 1, 0]))
-            .addPolygon(new Polygon([3, 2, 7, 6]));
+        if (allTriangles) {
+            this
+                .addPolygon(new Polygon([0, 1, 2, 0]))
+                .addPolygon(new Polygon([0, 2, 3, 0]))
+                .addPolygon(new Polygon([3, 5, 0, 3]))
+                .addPolygon(new Polygon([3, 6, 5, 3]))
+                .addPolygon(new Polygon([4, 5, 6, 4]))
+                .addPolygon(new Polygon([4, 6, 7, 4]))
+                .addPolygon(new Polygon([1, 4, 7, 1]))
+                .addPolygon(new Polygon([1, 7, 2, 1]))
+                .addPolygon(new Polygon([5, 4, 1, 5]))
+                .addPolygon(new Polygon([5, 1, 0, 5]))
+                .addPolygon(new Polygon([3, 2, 7, 3]))
+                .addPolygon(new Polygon([3, 7, 6, 3]));
+        } else {
+            this
+                .addPolygon(new Polygon([0, 1, 2, 3]))
+                .addPolygon(new Polygon([0, 3, 6, 5]))
+                .addPolygon(new Polygon([4, 5, 6, 7]))
+                .addPolygon(new Polygon([1, 4, 7, 2]))
+                .addPolygon(new Polygon([5, 4, 1, 0]))
+                .addPolygon(new Polygon([3, 2, 7, 6]));
+        }
 
         this.calcNormals();
     }
