0665551a8bd9c49979c8799b7c326c5d0e25064d
Juan García <jgarcia@bodas.net>
2022-04-27 12:19:45 +0200

    feat: draw plain using squares

--- js/geometries/Plane.js
+++ js/geometries/Plane.js
@@ -12,16 +12,7 @@ class Plane extends Geometry {
                 this.addPoint(new Point(j * size, 0, i * size));
                 if (j < space && i < space) {
                     let bj = b + j;
-                    this.addPolygon(new Polygon([
-                        bj,
-                        bj + 1,
-                        bj + space + 1]
-                    ));
-                    this.addPolygon(new Polygon([
-                        bj + 1,
-                        bj + space + 2,
-                        bj + space + 1]
-                    ));
+                    this.addPolygon(new Polygon([bj + 1, bj + space + 2, bj + space + 1, bj]));
                 }
             }
         }
